35a1d97632ef4e761c4478e206c610c9dacbfd1d
curtis <375577020@qq.com>
2021-12-15 23:14:47 +0800

    提交熔断服务相关代码

---
+++ cloud-consumer-order-hystrix-9101/src/main/java/com/curtis/springcloud/consumer/order/hystrix/controller/TimeoutController.java
@@ -0,0 +1,33 @@
+package com.curtis.springcloud.consumer.order.hystrix.controller;
+
+import com.curtis.springcloud.consumer.order.hystrix.common.CommonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class TimeoutController {
+
+    // @LoadBalanced
+    @Resource
+    private RestTemplate restTemplate;
+
+    // private static final String PAYMENT_SERVICE_URL = "http://localhost:8001";
+    // 这里服务名称与大小写无关，均可
+    private static final String PAYMENT_SERVICE_URL = "http://CLOUD-PROVIDER-PAYMENT-SERVICE";
+    // private static final String PAYMENT_SERVICE_URL = "http://cloud-provider-payment-service";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimeoutController.class);
+
+
+    @GetMapping(value = "/timeout/{second}")
+    public CommonResult timeout(@PathVariable("second") Integer second) {
+        CommonResult commonResult = restTemplate.getForObject(PAYMENT_SERVICE_URL + "/timeout/" + second, CommonResult.class);
+        return commonResult;
+    }
+}
---
+++ cloud-provider-payment-8002/src/main/java/com/curtis/springcloud/provider/payment/controller/ExceptionController.java
@@ -0,0 +1,25 @@
+package com.curtis.springcloud.provider.payment.controller;
+
+import com.curtis.springcloud.provider.payment.common.CommonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
+
+
+    @GetMapping(value = "/exception/{param}")
+    public CommonResult exception(@PathVariable String param) {
+        LOGGER.info("receive request");
+        throw new RuntimeException("An exception occurs, server port: " + serverPort + ", The PathVariable Is " + param);
+    }
+}
