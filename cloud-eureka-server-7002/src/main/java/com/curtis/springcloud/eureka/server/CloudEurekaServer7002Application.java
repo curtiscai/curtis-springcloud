16531290466fd834578b6d0bda4c0dfa3eaa3124
curtis <375577020@qq.com>
2021-12-20 20:20:24 +0800

    调整服务治理中心及生产端、消费端配置

--- cloud-eureka-server-7002/src/main/java/com/curtis/springcloud/eureka/server/CloudEurekaServer7002Application.java
+++ cloud-eureka-server-7002/src/main/java/com/curtis/springcloud/eureka/server/CloudEurekaServer7002Application.java
@@ -1,16 +1,19 @@
-package com.curtis.springcloud.eureka.server.server;
+package com.curtis.springcloud.eureka.server;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 集群版服务注册中心
+ */
 // 启用Eureka服务治理中心（activate Eureka Server related configuration）
 @EnableEurekaServer
 @SpringBootApplication
-public class CloudEurekaServer7001Application {
+public class CloudEurekaServer7002Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudEurekaServer7001Application.class, args);
+        SpringApplication.run(CloudEurekaServer7002Application.class, args);
     }
 }
--- cloud-provider-payment-8002/src/main/java/com/curtis/springcloud/provider/payment/CloudProviderPayment8002Application.java
+++ cloud-provider-payment-8002/src/main/java/com/curtis/springcloud/provider/payment/CloudProviderPayment8002Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+// 使用@EnableDiscoveryClient或者@EnableEurekaClient都可以启用Eureka服务发现，H版中不需要该注解也可以启用Eureka服务发现
+// @EnableDiscoveryClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class CloudProviderPayment8002Application {
 
